d8ade4dca1655e9acbc37a75133f4f03eb1fbd4b
MatheAndrei <matheandrei13@gmail.com>
2024-04-07 15:07:47 +0300

    Added all test for TemaValidator

--- src/test/java/org/example/TestTemaValidator.java
+++ src/test/java/org/example/TestTemaValidator.java
@@ -17,19 +17,58 @@ public class TestTemaValidator {
 	}
 
 	@Test
-	public void testValidate_good() {
+	public void testValidate_1() {
 		Tema tema = new Tema("1", "string", 2, 1);
 		validator.validate(tema);
 	}
 
 	@Test
-	public void testValidate_bad() {
+	public void testValidate_2() {
 		Tema tema = new Tema("", "string", 2, 1);
 		Exception exception = Assert.assertThrows(ValidationException.class, () -> {
 			validator.validate(tema);
 		});
 
 		String expectedMessage = "ID invalid! \n";
+		String actualMessage = exception.getMessage();
+
+		Assert.assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	public void testValidate_3() {
+		Tema tema = new Tema("1", "", 2, 1);
+		Exception exception = Assert.assertThrows(ValidationException.class, () -> {
+			validator.validate(tema);
+		});
+
+		String expectedMessage = "Descriere invalida! \n";
+		String actualMessage = exception.getMessage();
+
+		Assert.assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	public void testValidate_4() {
+		Tema tema = new Tema("1", "string", 0, 1);
+		Exception exception = Assert.assertThrows(ValidationException.class, () -> {
+			validator.validate(tema);
+		});
+
+		String expectedMessage = "Deadline invalid! \n";
+		String actualMessage = exception.getMessage();
+
+		Assert.assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	public void testValidate_5() {
+		Tema tema = new Tema("1", "string", 2, 0);
+		Exception exception = Assert.assertThrows(ValidationException.class, () -> {
+			validator.validate(tema);
+		});
+
+		String expectedMessage = "Data de primire invalida! \n";
 		String actualMessage = exception.getMessage();
 
 		Assert.assertEquals(expectedMessage, actualMessage);
