60059e85f14b4313d651e4cd16a79f5571f3a4b7
MatheAndrei <matheandrei13@gmail.com>
2024-04-21 22:51:36 +0300

    Added incremental integration tests

--- src/test/java/service/ServiceTest_BigBang.java
+++ src/test/java/service/ServiceTest_BigBang.java
@@ -10,9 +10,7 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.Validator;
 
-import static org.mockito.Mockito.mock;
-
-public class ServiceTest extends TestCase {
+public class ServiceTest_BigBang extends TestCase {
 	private Service service;
 
 	public void setUp() {
---
+++ src/test/java/service/ServiceTest_Incremental.java
@@ -0,0 +1,98 @@
+package service;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import junit.framework.TestCase;
+import repository.NotaRepository;
+import repository.StudentRepository;
+import repository.TemaRepository;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.Assert.*;
+
+public class ServiceTest_Incremental extends TestCase {
+	private Service service;
+
+	public void setUp() {
+		Validator<Student> studentValidator = new StudentValidator();
+		Validator<Tema> temaValidator = new TemaValidator();
+		Validator<Nota> notaValidator = new NotaValidator();
+
+		StudentRepository repo1 = new StudentRepository(studentValidator);
+		TemaRepository repo2 = new TemaRepository(temaValidator);
+		NotaRepository repo3 = new NotaRepository(notaValidator);
+
+		service = new Service(repo1, repo2, repo3);
+	}
+
+	public void testSaveStudent() {
+		int result = service.saveStudent("1", "bla", 111);
+		assertEquals(result, 1);
+
+		result = service.saveStudent(null, "bla", 111);
+		assertEquals(result, 0);
+
+		result = service.saveStudent("1", null, 111);
+		assertEquals(result, 0);
+
+		result = service.saveStudent("1", "bla", 1);
+		assertEquals(result, 0);
+	}
+
+	public void testSaveStudentTema() {
+		// student
+		int result = service.saveStudent("1", "bla", 111);
+		assertEquals(result, 1);
+
+		result = service.saveStudent(null, "bla", 111);
+		assertEquals(result, 0);
+
+		result = service.saveStudent("1", null, 111);
+		assertEquals(result, 0);
+
+		result = service.saveStudent("1", "bla", 1);
+		assertEquals(result, 0);
+
+		// tema
+		result = service.saveTema("1", "fjfkdkd", 2, 2);
+		assertEquals(result, 1);
+
+		result = service.saveTema(null, "fjfkdkd", 2, 2);
+		assertEquals(result, 0);
+
+		result = service.saveTema("1", null, 2, 2);
+		assertEquals(result, 0);
+
+		result = service.saveTema("1", "fjfkdkd", 3, 6);
+		assertEquals(result, 0);
+
+		result = service.saveTema("1", "fjfkdkd", 2, 0);
+		assertEquals(result, 0);
+
+		result = service.saveTema("1", "fjfkdkd", 15, 2);
+		assertEquals(result, 0);
+	}
+
+	public void testSaveStudentTemaNota() {
+		int result = service.saveNota("1", "1", 3, 3, "ddhhf");
+		assertEquals(result, -1);
+
+		result = service.saveStudent("1", "bla", 111);
+		assertEquals(result, 1);
+		result = service.saveTema("1", "fjfkdkd", 2, 2);
+		assertEquals(result, 1);
+
+		result = service.saveNota("1", "1", 3, 3, "ddhhf");
+		assertEquals(result, 1);
+
+		result = service.saveNota("1", "1", 3, -1, "ddhhf");
+		assertEquals(result, 0);
+
+		result = service.saveNota("1", "1", 11, 2, "ddhhf");
+		assertEquals(result, 0);
+	}
+}
